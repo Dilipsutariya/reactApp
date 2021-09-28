c5db36ad2df7f37262c5966da2b65e6b5a5ba728
dilip sutariya <64394652+Dilipsutariya@users.noreply.github.com>
2021-09-28 16:42:07 +1000

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 from flask import Flask
-import json
+# import json
 from flask import request
 from datetime import datetime
 import psycopg2
@@ -21,67 +21,67 @@ def hello():
     data="123"
     return data
 
-@app.route('/getUsers', methods = ['GET'])
-def getUsers():
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM vw_users')
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    return json.dumps(results)
+# @app.route('/getUsers', methods = ['GET'])
+# def getUsers():
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM vw_users')
+#     columns = [column[0] for column in cursor.description]
+#     results = []
+#     for row in cursor.fetchall():
+#         results.append(dict(zip(columns, row)))
+#     return json.dumps(results)
 
-@app.route('/getEventDates', methods = ['GET'])
-def getEventDates():
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM tbl_eventDates')
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    return json.dumps(results)
+# @app.route('/getEventDates', methods = ['GET'])
+# def getEventDates():
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM tbl_eventDates')
+#     columns = [column[0] for column in cursor.description]
+#     results = []
+#     for row in cursor.fetchall():
+#         results.append(dict(zip(columns, row)))
+#     return json.dumps(results)
 
-@app.route('/setEvents', methods = ['POST'])
-def setEvents():
-    cursor = conn.cursor()
-    insert_records = '''INSERT INTO tbl_events (eventDate_id,user_id)
-                VALUES(?,?) '''
-    cursor.execute(insert_records,request.args['eventDate_id'],request.args['user_id'])
-    conn.commit()
-    return 'Inserted'
+# @app.route('/setEvents', methods = ['POST'])
+# def setEvents():
+#     cursor = conn.cursor()
+#     insert_records = '''INSERT INTO tbl_events (eventDate_id,user_id)
+#                 VALUES(?,?) '''
+#     cursor.execute(insert_records,request.args['eventDate_id'],request.args['user_id'])
+#     conn.commit()
+#     return 'Inserted'
 
-@app.route('/setUsers', methods = ['POST'])
-def setUsers():
-    cursor = conn.cursor()
-    insert_records = '''INSERT INTO bt_interviews.dbo.tbl_users (Name)
-                VALUES(?) '''
-    cursor.execute(insert_records,request.args['name'])
-    conn.commit()
-    cursor.execute('SELECT Top 1 id FROM tbl_users ORDER BY entered_date DESC')
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    return json.dumps(results)
+# @app.route('/setUsers', methods = ['POST'])
+# def setUsers():
+#     cursor = conn.cursor()
+#     insert_records = '''INSERT INTO bt_interviews.dbo.tbl_users (Name)
+#                 VALUES(?) '''
+#     cursor.execute(insert_records,request.args['name'])
+#     conn.commit()
+#     cursor.execute('SELECT Top 1 id FROM tbl_users ORDER BY entered_date DESC')
+#     columns = [column[0] for column in cursor.description]
+#     results = []
+#     for row in cursor.fetchall():
+#         results.append(dict(zip(columns, row)))
+#     return json.dumps(results)
 
-@app.route('/getEvents', methods = ['GET'])
-def getEvents():
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM tbl_events')
-    columns = [column[0] for column in cursor.description]
-    results = []
-    for row in cursor.fetchall():
-        results.append(dict(zip(columns, row)))
-    return json.dumps(results)
+# @app.route('/getEvents', methods = ['GET'])
+# def getEvents():
+#     cursor = conn.cursor()
+#     cursor.execute('SELECT * FROM tbl_events')
+#     columns = [column[0] for column in cursor.description]
+#     results = []
+#     for row in cursor.fetchall():
+#         results.append(dict(zip(columns, row)))
+#     return json.dumps(results)
 
-@app.route('/setEventDates', methods = ['POST'])
-def setEventDates():
-    cursor = conn.cursor()
-    insert_records = '''INSERT INTO tbl_eventDates (eventDate,eventTime)
-                VALUES(?,?) '''
-    cursor.execute(insert_records,datetime.strptime(request.args['eventDate'],'%d/%m/%y'),datetime.strptime(request.args['eventTime'],'%H:%M:%S'))
-    conn.commit()
-    return 'Inserted'
+# @app.route('/setEventDates', methods = ['POST'])
+# def setEventDates():
+#     cursor = conn.cursor()
+#     insert_records = '''INSERT INTO tbl_eventDates (eventDate,eventTime)
+#                 VALUES(?,?) '''
+#     cursor.execute(insert_records,datetime.strptime(request.args['eventDate'],'%d/%m/%y'),datetime.strptime(request.args['eventTime'],'%H:%M:%S'))
+#     conn.commit()
+#     return 'Inserted'
 
 if __name__ == '__main__':
     app.run()
